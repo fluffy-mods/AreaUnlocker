174dfc193d799f15c96d9c15f58ed4e0e5737096
FluffierThanThou <fluffy.l2032@gmail.com>
2016-07-15 22:16:20 +0200

    Update to Alpha 14

--- Source/AreaUnlocker/SpecialInjector_AreaUnlocker.cs
+++ Source/AreaUnlocker/SpecialInjector_AreaUnlocker.cs
@@ -6,81 +6,24 @@
 
 using System;
 using System.Reflection;
+using CommunityCoreLibrary;
 using RimWorld;
 using Verse;
 
 namespace AreaUnlocker
 {
-    public class Bootstrap : ITab
+    public class DetourInjector : SpecialInjector
     {
-        public bool NEW_AREA_ALLOWED_OVERRIDE = true;
-
-        /// <summary>
-        /// Edits assembly code to always return true when AreaManager.CanMakeNewAllowed() is called.
-        /// I don't actually fully understand how this works, so for questions contact RawCode on the Ludeon forums.
-        /// 
-        /// All credits go to RawCode, who created this code for overriding combat slowdown;
-        /// https://ludeon.com/forums/index.php?topic=16774.0
-        /// </summary>
-        public void OverrideMethod()
+        public override bool Inject()
         {
-            try
-            {
-                unsafe
-                {
-                    //getting pointer to method
-                    //double check binding flags, in other case game will fail with NPE
-                    byte* mpx_1 =
-                        (byte*)
-                            typeof (AreaManager).GetMethod( "CanMakeNewAllowed",
-                                                            BindingFlags.Instance | BindingFlags.Public )
-                                                .MethodHandle.GetFunctionPointer()
-                                                .ToPointer();
 
-                    //lazy init
-                    int ttz = 0;
+            MethodInfo source = typeof( AreaManager ).GetMethod( "CanMakeNewAllowed", BindingFlags.Instance | BindingFlags.Public );
+            MethodInfo destination = typeof( DetourInjector ).GetMethod( "CanMakeNewAllowed", BindingFlags.Instance | BindingFlags.Public );
 
-                    //read reference of our controller variable
-                    fixed (bool* key = &NEW_AREA_ALLOWED_OVERRIDE)
-                    {
-                        //cast reference into int
-                        ttz = (int)key;
-                    }
-
-                    //just debug
-                    Log.Warning( ttz.ToString( "X2" ) );
-
-                    //convert reference into byte array, this required to "reverse" bytes
-                    byte[] bytes = BitConverter.GetBytes( ttz );
-
-                    *( mpx_1 + 0 ) = 0xB8; //XOR MOV RAW
-                    *( mpx_1 + 1 ) = bytes[0]; //RAW WORD
-                    *( mpx_1 + 2 ) = bytes[1];
-                    *( mpx_1 + 3 ) = bytes[2];
-                    *( mpx_1 + 4 ) = bytes[3];
-
-                    *( mpx_1 + 5 ) = 0x8b; //XOR DEREFERENCE
-                    *( mpx_1 + 6 ) = 0; //into itself
-                    *( mpx_1 + 7 ) = 0x90; //padding
-                    *( mpx_1 + 8 ) = 0x90;
-
-                    *( mpx_1 + 9 ) = 0xC3; //return
-                }
-            }
-            catch
-            {
-                Log.Warning( "Area unlocker method override failed." );
-            }
+            return Detours.TryDetourFromTo( source, destination );
         }
 
-        public Bootstrap()
-        {
-            LongEventHandler.ExecuteWhenFinished( delegate { OverrideMethod(); } );
-        }
+        public bool CanMakeNewAllowed( AreaManager _this, AllowedAreaMode mode ) { return true; }
 
-        protected override void FillTab()
-        {
-            // required implementation.
-        }
     }
 }
