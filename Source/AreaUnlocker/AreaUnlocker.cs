2e29e9da6bc067132aed2f1265fe0fdd212b8900
FluffierThanThou <fluffy.l2032@gmail.com>
2016-12-21 18:42:00 +0100

    Update for A16
    Move from CCL to HugsLib

---
+++ Source/AreaUnlocker/AreaUnlocker.cs
@@ -0,0 +1,20 @@
+﻿// AreaUnlocker/SpecialInjector_AreaUnlocker.cs
+// 
+// Copyright Karel Kroeze, 2015.
+// 
+// Created 2015-11-25 10:55
+
+using System;
+using HugsLib.Source.Detour;
+using RimWorld;
+using Verse;
+
+namespace AreaUnlocker
+{
+    public class AreaUnlocker
+    {
+        // smallest mod ever
+        [DetourMethod(typeof(AreaManager), "ConMakeNewAllowed" )]
+        public bool CanMakeNewAllowed( AreaManager _this, AllowedAreaMode mode ) { return true; }
+    }
+}
---
+++ Source/description.md
@@ -0,0 +1,7 @@
+Removes the (arbitrary) limit to the number of areas you can create. 
+
+# Important
+Requires HugsLib (See link on the right)
+
+# Notes
+While this completely removes the limit on the number of areas you can create, the UI is untouched - and optimized for 5 areas per type. If you create too many areas, you may run into UI issues. This practical limit depends largely on your resolution. I have no plans to update these UI issues, as that would be a massive undertaking.
